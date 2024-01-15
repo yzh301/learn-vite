88f0012f402a2b306ed888b23e822b01a0609240
WuDaoTingFeng.yzh <2683849644@qq.com>
2024-01-15 17:30:35 +0800

    使用 scss ,使用 autoprefiexer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
+        }),
+      ]
+    }
+  },
   plugins: [vue()],
 })
